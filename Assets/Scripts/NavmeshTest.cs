0c2281112db84fd38533c5c4dbe6e725999023b9
Daniel Samuelsson <samuda@utb.fuv.se>
2016-02-12 13:57:41 +0100

    Daniel has fun!

---
+++ Assets/Scripts/NavmeshTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class NavmeshTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake() {
+        NavMeshBuilder.BuildNavMesh();
+        Debug.Log("RUNNING");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
