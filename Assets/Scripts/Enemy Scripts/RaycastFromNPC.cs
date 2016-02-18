f1f78986f180c211196abbd4852fb41729070c92
Daniel Samuelsson <samuda@utb.fuv.se>
2016-02-18 17:10:40 +0100

    Daniel är seg.

---
+++ Assets/Scripts/Enemy Scripts/RaycastFromNPC.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastFromNPC : MonoBehaviour
+{
+
+    private float m_Distance = 20.0f;
+	void Start ()
+    {
+	
+	}
+
+	void Update ()
+    {
+        //Ray ray = gameObject.tag("Ghoul");
+        //RaycastHit hitInfo;
+
+        //if(Physics.Raycast(ray, out hitInfo, m_Distance)
+//        {
+
+  //      }
+
+    }
+}
